04427658602f542cfd90e6965af83929bb357420
CARRAFRAGA <cracklosramirez@hotmail.com>
2022-02-06 18:48:45 -0300

    se crea el menu de navegacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Barranav from './components/Barranav/Barranav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Barranav/>
     </div>
   );
 }
---
+++ src/components/Barranav/Barranav.js
@@ -0,0 +1,31 @@
+import "./Barranav.css"
+
+const Barranav= () =>{
+    return (
+      <nav>
+              <div>
+      <img src={"./images/pacman.jpg"} className= "logo" alt="logo"/>
+    </div>
+          <div>
+          <h1>
+     GAME STORE
+     </h1>
+    </div>
+       <button>
+         Consolas Retro
+       </button>
+       <button>
+         Consolas actuales
+       </button>
+       <button>
+         Periféricos
+       </button>
+       <button>
+         Juegos
+       </button>
+      </nav>
+  
+    )
+  }
+
+  export default Barranav
